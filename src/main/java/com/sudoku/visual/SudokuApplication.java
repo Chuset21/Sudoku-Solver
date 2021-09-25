a97bc85a8f4e8e3ff27f4f8f7c5c0164d4a43217
Miguel <miguelarrieta02@gmail.com>
2021-09-25 19:51:42 +0100

    Got a semi-working sudoku grid

--- src/main/java/com/sudoku/visual/SudokuApplication.java
+++ src/main/java/com/sudoku/visual/SudokuApplication.java
@@ -1,23 +1,51 @@
 package com.sudoku.visual;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.css.PseudoClass;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class SudokuApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SudokuApplication.class.getResource("sudoku-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage primaryStage) {
+        final GridPane board = new GridPane();
+
+        final PseudoClass right = PseudoClass.getPseudoClass("right");
+        final PseudoClass bottom = PseudoClass.getPseudoClass("bottom");
+
+        for (int col = 0; col < 9; col++) {
+            for (int row = 0; row < 9; row++) {
+                final StackPane cell = new StackPane();
+                cell.getStyleClass().add("cell");
+                cell.pseudoClassStateChanged(right, col == 2 || col == 5);
+                cell.pseudoClassStateChanged(bottom, row == 2 || row == 5);
+
+                cell.getChildren().add(createTextField());
+
+                board.add(cell, col, row);
+            }
+        }
+
+
+        final Scene scene = new Scene(board);
+        scene.getStylesheets().add("sudoku.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private TextField createTextField() {
+        final TextField textField = new TextField();
+
+        // restrict input to integers:
+        textField.setTextFormatter(new TextFormatter<>(c -> c.getControlNewText().matches("\\d?") ? c : null));
+        return textField;
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
